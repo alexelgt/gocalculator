46887d732ad756ed53cf3422de2706f96d447954
alexelgt <alexelgt@users.noreply.github.com>
2019-03-17 16:14:07 +0100

    Test 4

--- sw.js
+++ sw.js
@@ -1,31 +1,17 @@
-var CACHE_NAME = 'my-site-cache-v1';
-var urlsToCache = [
-  '/',
-  '/styles/main.css',
-  '/script/main.js',
-  'logo.png',
-  'index.html'
-];
 self.addEventListener('install', function(event) {
-  // Perform install steps
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(
+        [
+          '/css/bootstrap.css',
+          '/css/main.css',
+          '/js/bootstrap.min.js',
+          '/js/jquery.min.js',
+          '/offline.html',
+          'logo.png',
+          'index.html'
+        ]
+      );
+    })
   );
 });
