66ba7d7cad84aec87e81d1c20bdf73b211fdf8e7
alexelgt <alexelgt@users.noreply.github.com>
2019-03-17 16:20:28 +0100

    Test 7

--- sw.js
+++ sw.js
@@ -4,11 +4,6 @@ self.addEventListener('install', function(event) {
     caches.open(cacheName).then(function(cache) {
       return cache.addAll(
         [
-          '/css/bootstrap.css',
-          '/css/main.css',
-          '/js/bootstrap.min.js',
-          '/js/jquery.min.js',
-          '/offline.html',
           '/logo.png',
           '/index.html'
         ]
