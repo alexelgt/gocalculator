3a4210e933c42ea29e87b762657be3b021d888b0
Alexelgt <alexelgt@MacBook-Pro-de-Alejandro.local>
2019-06-04 13:38:06 +0200

    Service worked removed

--- javascript.js
+++ javascript.js
@@ -16,17 +16,6 @@ function start(){
       document.getElementById("Output_PVP_Stats_textarea").selectionEnd = 9999
   });
 
-  if ('serviceWorker' in navigator) {
-    console.log('CLIENT: service worker registration in progress.');
-    navigator.serviceWorker.register('service-worker.js').then(function() {
-      console.log('CLIENT: service worker registration complete.');
-    }, function() {
-      console.log('CLIENT: service worker registration failure.');
-    });
-  } else {
-    console.log('CLIENT: service worker is not supported.');
-  }
-
 }
 
 function ventanaemergente(id,id2) {
